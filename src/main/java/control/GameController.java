a61886cf930a2573a2d6ff6821d02dcbb9a10128
Alexander <alexander@rutz.tv>
2026-01-05 20:50:56 +0100

    feat(control): basic implementation of main app and MVC controllers

--- src/main/java/control/GameController.java
+++ src/main/java/control/GameController.java
@@ -1,5 +1,22 @@
 package control;
 
-public class GameController {
+import model.HangmanGame;
+import model.QuestionPool;
+import model.Question;
+import java.util.Collections;
+import java.util.List;
 
+public class GameController {
+    public GameController() {}
+
+    public HangmanGame newGameFromPool(QuestionPool pool) {
+        List<Question> questions = pool.getQuestions();
+        if (questions.isEmpty()) {
+            throw new RuntimeException("Pool is empty");
+        }
+
+        Collections.shuffle(questions);
+        String targetWord = questions.get(0).getAnswer();
+        return new HangmanGame(10, targetWord);
+    }
 }

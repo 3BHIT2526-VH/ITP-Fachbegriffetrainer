9abbb7bcfb94a345b46ac182d5c2ff09656237cc
H4WKF0X <166375792+H4WKF0X@users.noreply.github.com>
2026-01-05 21:21:40 +0100

    feat(model): implement QuestionPool and Resultitem (#8)
    Super

--- src/main/java/model/ResultItem.java
+++ src/main/java/model/ResultItem.java
@@ -1,5 +1,22 @@
 package model;
 
-public class ResultItem {
+import java.util.UUID;
 
+public class ResultItem {
+    private UUID questionId;
+    private boolean correct;
+    private String userAnswer;
+    private String expectedAnswer;
+
+    public ResultItem(UUID questionId, boolean correct, String userAnswer, String expectedAnswer) {
+        this.questionId = questionId;
+        this.correct = correct;
+        this.userAnswer = userAnswer;
+        this.expectedAnswer = expectedAnswer;
+    }
+
+    public boolean isCorrect() { return correct; }
+    public UUID getQuestionId() { return questionId; }
+    public String getUserAnswer() { return userAnswer; }
+    public String getExpectedAnswer() { return expectedAnswer; }
 }

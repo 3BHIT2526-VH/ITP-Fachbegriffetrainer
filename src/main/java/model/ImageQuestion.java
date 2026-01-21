fe9294b5cb4d028f6240f97482247abba3f7879e
Leddis08 <65092977+leddis088@users.noreply.github.com>
2026-01-21 15:13:03 +0100

    feat(model): implement question logic

--- src/main/java/model/ImageQuestion.java
+++ src/main/java/model/ImageQuestion.java
@@ -1,5 +1,22 @@
 package model;
 
-public class ImageQuestion {
+import java.net.URL;
+import java.util.UUID;
 
+public class ImageQuestion extends Question {
+    private URL imageURL;
+
+    public ImageQuestion(UUID id, String prompt, String answer, URL imageURL) {
+        super(id, prompt, answer);
+        this.imageURL = imageURL;
+    }
+
+    public URL getImageUrl() { return imageURL; }
+
+    @Override
+    public void validate() {
+        if (imageURL == null) {
+            throw new RuntimeException("Image URL is required for ImageQuestion");
+        }
+    }
 }

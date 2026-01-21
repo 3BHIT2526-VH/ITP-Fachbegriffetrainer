aec8bbaeda6196c1d72b6d28ef68b80f92c7418b
H4WKF0X <166375792+H4WKF0X@users.noreply.github.com>
2026-01-21 15:25:13 +0100

    feat(model): implement question logic (#10)
    Co-authored-by: Leddis08 <65092977+leddis088@users.noreply.github.com>

--- src/main/java/model/Question.java
+++ src/main/java/model/Question.java
@@ -1,6 +1,21 @@
 package model;
 
+import java.util.UUID;
 
 public abstract class Question {
+    protected UUID id;
+    protected String prompt;
+    protected String answer;
 
+    protected Question(UUID id, String prompt, String answer) {
+        this.id = id;
+        this.prompt = prompt;
+        this.answer = answer;
+    }
+
+    public String getPrompt() { return prompt; }
+    public String getAnswer() { return answer; }
+    public UUID getId() { return id; }
+
+    public abstract void validate();
 }
--- src/main/java/model/TextQuestion.java
+++ src/main/java/model/TextQuestion.java
@@ -1,5 +1,16 @@
 package model;
 
-public class TextQuestion{
+import java.util.UUID;
 
+public class TextQuestion extends Question {
+    public TextQuestion(UUID id, String prompt, String answer) {
+        super(id, prompt, answer);
+    }
+
+    @Override
+    public void validate() {
+        if (prompt == null || prompt.isBlank() || answer == null || answer.isBlank()) {
+            throw new RuntimeException("TextQuestion components cannot be blank");
+        }
+    }
 }
